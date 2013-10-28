dfeadb3563e4ee94bbe545bd0ad91d88debab726
jcreixell <jcreixell@gmail.com>
2013-10-28 16:33:11 +0100

    Add configuration options for Redis in production

--- config.rb
+++ config.rb
@@ -1,3 +1,10 @@
-config['redis'] = EM::Synchrony::ConnectionPool.new size: 2 do
-  Redis.new
+if Goliath.env?(:production)
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  config['redis'] = EM::Synchrony::ConnectionPool.new size: 2 do
+    Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
+else
+  config['redis'] = EM::Synchrony::ConnectionPool.new size: 2 do
+    Redis.new
+  end
 end
