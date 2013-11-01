cb35fa55cf63c0cf0cfafd8c7e6e028a4b972448
jcreixell <jcreixell@gmail.com>
2013-11-01 13:30:32 +0100

    Improve naming and codebase structure

---
+++ lib/contentful-sync-rss.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
+
+# load dependencies
+require 'goliath'
+require 'em-synchrony/em-http'
+require 'redis'
+require 'json'
+require 'yaml'
+
+# load app
+require 'contentful-sync-rss/errors'
